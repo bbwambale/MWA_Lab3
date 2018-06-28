39395f4fbefa1522c3c3e9f471cf9cb1d9056f48
bbwambale <basiyonah033@gmail.com>
2018-06-27 23:57:39 -0500

    Web Server Demo

---
+++ Exercise2WebServer.js
@@ -0,0 +1,30 @@
+/*var http = require('http');
+var server = http.createServer();
+
+server.on('request', function(req, res){
+        res.writeHead(200, {'Content-Type':'text/plain'});
+        res.write('Hello World!');
+        res.end();
+});
+server.listen(4000);
+*/
+var http = require('http'),
+    fs = require('fs'),
+    url = require('url');
+
+    http.createServer(function(req, res){
+            var query = url.parse(req.url, true).query;
+            pic = query.image;
+
+            fs.readFile('/couple.jpg', + pic, function(err,content ){
+                    if(err){
+                        res.writeHead(400, {'Content-Type':'text/html'})
+                        console.log(err);
+                        res.end("No such Image");
+                    }else{
+                            res.writeHead(200, {'Content-Type':'image/jpg'});
+                            res.end(content);
+                    }
+            });
+    }).listen(50000);
+    console.log("Server running at http://localhost:4444");
